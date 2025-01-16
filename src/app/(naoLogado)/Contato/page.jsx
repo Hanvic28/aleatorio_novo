694ec056094ad16692dc0d1e925517f6d56ccce8
Hanvic28 <luisotaviomkta@gmail.com>
2025-01-16 16:51:34 -0300

    feat: outra alteração na visualização do tel

--- src/app/(naoLogado)/Contato/page.jsx
+++ src/app/(naoLogado)/Contato/page.jsx
@@ -1,7 +1,7 @@
 export default function Sobre() {
 	return (
 		<div className="texto">
-			<h1>Whatsapp: (32) 999003559 / telefone: (32) 99473-2624</h1>
+			<h1>Whatsapp: (32) 99900-3559 / telefone: (32) 99473-2624</h1>
 		</div>
 	);
 }
